c806ba80f60dcfd2e74bbaf52f25cbd82127640f
Gustavo Bacagine <gustavo.bacagine@protonmail.com>
2025-08-02 10:43:48 -0300

    Update date_time.h

--- include/cutils/date_time.h
+++ include/cutils/date_time.h
@@ -42,14 +42,25 @@ typedef struct STRUCT_TIME
 
 typedef enum ENUM_DATE_FMT
 {
-  DDMMAA,
-  DDMMAAAA,
-  AAAAMMDD,
-  AAMMDD,
-  MMDDAAAA,
-  MMDDAA,
+  DDMMYY,
+  DDMMYYYY,
+  YYYYMMDD,
+  YYMMDD,
+  MMDDYYYY,
+  MMDDYY,
   FMTERROR, 
 } ENUM_DATE_FMT;
+
+/**
+ * @enum ENUM_TIME_FMT
+ * @brief Time format enumeration for string formatting/parsing.
+ */
+typedef enum ENUM_TIME_FMT {
+  HHMM,
+  HHMMSS,
+  HHMMSSM,
+  TIME_FMTERROR
+} ENUM_TIME_FMT;
 
 /**
  * Date formates string array
@@ -121,6 +132,57 @@ void vFormatDate(const STRUCT_DATE *kpstDate,
  * Print date in specific format
  */
 void vPrintDate(PSTRUCT_DATE pstDate, const char *kpszFmt);
+
+/**
+ * @brief Parses a date string into a STRUCT_DATE based on the specified format and separator.
+ *
+ * Extracts day, month, and year from the input string and stores them in the date structure.
+ *
+ * @param pstDate Pointer to the date structure to populate.
+ * @param kpszDate Input date string.
+ * @param pszSeparator Separator used in the date string.
+ * @param eDateFmt Date format enumeration that matches the input format.
+ */
+void vGetDateFromStr(PSTRUCT_DATE pstDate, const char *kpszDate, char *pszSeparator, ENUM_DATE_FMT eDateFmt);
+
+/**
+ * @brief Formats a time structure into a string according to the specified format.
+ *
+ * Converts the given STRUCT_TIME into a formatted string based on the time format enumeration.
+ *
+ * @param kpszTime Pointer to the time structure to format.
+ * @param eTimeFmt Time format enumeration specifying output format.
+ * @param pszOutput Buffer to receive the formatted time string.
+ * @param lOutputSize Size of the output buffer.
+ */
+void vFormatTime(
+  const PSTRUCT_TIME kpszTime,
+  const ENUM_TIME_FMT eTimeFmt,
+  char *pszOutput,
+  size_t lOutputSize);
+
+/**
+ * @brief Prints a time structure to standard output using the specified format.
+ *
+ * Outputs the formatted time to stdout.
+ *
+ * @param pstTime Pointer to the time structure to print.
+ * @param eTimeFmt Time format enumeration.
+ */
+void vPrintTime(PSTRUCT_TIME pstTime, ENUM_TIME_FMT eTimeFmt);
+
+/**
+ * @brief Prints a date and time combination to standard output.
+ *
+ * Formats and outputs the date and time according to the specified formats and separator.
+ *
+ * @param pstDate Pointer to the date structure.
+ * @param eDateFmt Date format enumeration.
+ * @param kpszDateSeparator Separator string between date components.
+ * @param pstTime Pointer to the time structure.
+ * @param eTimeFmt Time format enumeration.
+ */
+void vPrintDateTime(PSTRUCT_DATE pstDate, ENUM_DATE_FMT eDateFmt, const char *kpszDateSeparator, PSTRUCT_TIME pstTime, ENUM_TIME_FMT eTimeFmt);
 
 /**
  * Return the format was used in kpszFmt
