fc28644012a2a1689a264c211b301af557790e1a
Gustavo Bacagine <gustavo.bacagine@protonmail.com>
2025-08-02 10:37:56 -0300

    Update date_time.c

--- src/date_time.c
+++ src/date_time.c
@@ -180,93 +180,293 @@ bool bDatesIsEqual(STRUCT_DATE *pstDateOne, STRUCT_DATE *pstDateTwo)
   return false;
 }
 
-ENUM_DATE_FMT eMatchDateOutputFormat(const char *kpszFmt)
-{
+ENUM_DATE_FMT eMatchDateOutputFormat(const char *kpszFmt) {
   if ( bStrIsEmpty(kpszFmt) )
-    return atoi(gkpszDateFormat[FMTERROR]);
-
-  if ( !strcasecmp(kpszFmt, gkpszDateFormat[DDMMAA]) )
-    return DDMMAA;
-  else if ( !strcasecmp(kpszFmt, gkpszDateFormat[DDMMAAAA]) )
-    return DDMMAAAA;
-  else if ( !strcasecmp(kpszFmt, gkpszDateFormat[AAAAMMDD]) )
-    return AAAAMMDD;
-  else if ( !strcasecmp(kpszFmt, gkpszDateFormat[AAMMDD]  ) )
-    return AAMMDD;
-  else if ( !strcasecmp(kpszFmt, gkpszDateFormat[MMDDAAAA]) )
-    return MMDDAAAA;
-  else if ( !strcasecmp(kpszFmt, gkpszDateFormat[MMDDAA]  ) )
-    return MMDDAA;
-  
-  return atoi(gkpszDateFormat[FMTERROR]);
+    return atoi(gkpszDateFormat[DATE_FMTERROR]);
+  if ( !strcasecmp(kpszFmt, gkpszDateFormat[DDMMYY]) )
+    return DDMMYY;
+  else if ( !strcasecmp(kpszFmt, gkpszDateFormat[DDMMYYYY]) )
+    return DDMMYYYY;
+  else if ( !strcasecmp(kpszFmt, gkpszDateFormat[YYYYMMDD]) )
+    return YYYYMMDD;
+  else if ( !strcasecmp(kpszFmt, gkpszDateFormat[YYMMDD]  ) )
+    return YYMMDD;
+  else if ( !strcasecmp(kpszFmt, gkpszDateFormat[MMDDYYYY]) )
+    return MMDDYYYY;
+  else if ( !strcasecmp(kpszFmt, gkpszDateFormat[MMDDYY]  ) )
+    return MMDDYY;
+  return atoi(gkpszDateFormat[DATE_FMTERROR]);
 }
 
-void vFormatDate(const STRUCT_DATE *kpstDate,
-                 const char *kpszFmt,
-                 char *szOutput,
-                 char *pchDlm)
-{
-  ENUM_DATE_FMT eDateFmt;
-
-  if( szOutput == NULL || bStrIsEmpty(pchDlm) ) 
-  {
+void vFormatDate(
+  const STRUCT_DATE *kpstDate,
+  ENUM_DATE_FMT eDateFmt,
+  const char *kpszSeparator,
+  char *pszOutput,
+  size_t lOutputSize) {
+  if ( pszOutput == NULL || bStrIsEmpty(kpszSeparator) )
     return;
-  }
-
-  switch ((eDateFmt = eMatchDateOutputFormat(kpszFmt)))
-  {
-    case DDMMAAAA:
-    case DDMMAA:
-      sprintf(szOutput,
-          "%02d%c%02d%c%0*d",
-          kpstDate->iDay,
-          *pchDlm,
-          kpstDate->iMonth,
-          *pchDlm,
-          eDateFmt == DDMMAA ? 2 : 4,
-          eDateFmt == DDMMAA ? kpstDate->iYear%100 : kpstDate->iYear
-      );
-      break;    
-    case AAAAMMDD:
-    case AAMMDD:
-     sprintf(szOutput, 
-        "%0*d%c%02d%c%02d",
-        eDateFmt == AAMMDD ? 2 : 4 ,
-        eDateFmt == AAMMDD ? kpstDate->iYear%100 : kpstDate->iYear,
-        *pchDlm,
+  switch ( eDateFmt ) {
+    case DDMMYYYY:
+    case DDMMYY: {
+      snprintf(
+        pszOutput,
+        lOutputSize,
+        "%02d%c%02d%c%0*d",
+        kpstDate->iDay,
+        *kpszSeparator,
         kpstDate->iMonth,
-        *pchDlm,
+        *kpszSeparator,
+        eDateFmt == DDMMYY ? 2 : 4,
+        eDateFmt == DDMMYY ? kpstDate->iYear%100 : kpstDate->iYear
+      );
+      break;
+    }
+    case YYYYMMDD:
+    case YYMMDD: {
+      snprintf(
+        pszOutput,
+        lOutputSize,
+        "%0*d%c%02d%c%02d",
+        eDateFmt == YYMMDD ? 2 : 4 ,
+        eDateFmt == YYMMDD ? kpstDate->iYear%100 : kpstDate->iYear,
+	*kpszSeparator,
+        kpstDate->iMonth,
+        *kpszSeparator,
         kpstDate->iDay
       );
       break;
-    case MMDDAAAA:
-    case MMDDAA:
-     sprintf(szOutput, 
+    }
+    case MMDDYYYY:
+    case MMDDYY: {
+      snprintf(
+        pszOutput,
+        lOutputSize,
         "%02d%c%02d%c%0*d",
         kpstDate->iMonth,
-        *pchDlm,
+        *kpszSeparator,
         kpstDate->iDay,
-        *pchDlm,
-        eDateFmt == MMDDAA ? 2 : 4,
-        eDateFmt == MMDDAA ? kpstDate->iYear%100 : kpstDate->iYear
+        *kpszSeparator,
+        eDateFmt == MMDDYY ? 2 : 4,
+        eDateFmt == MMDDYY ? kpstDate->iYear%100 : kpstDate->iYear
       );
       break;
-    case FMTERROR:
+    }
+    case DATE_FMTERROR:
+    default: {
+      pszOutput = NULL;
+      return;
+    }
+  }
+}
+
+void vPrintDate(PSTRUCT_DATE pstDate, ENUM_DATE_FMT eDateFmt, const char *kpszSeparator) {
+  char szOutput[256] = "";
+  memset(szOutput, 0x00, sizeof(szOutput));
+  vFormatDate(pstDate, eDateFmt, kpszSeparator, szOutput, sizeof(szOutput));
+  puts(szOutput);
+}
+void vGetDateFromStr(PSTRUCT_DATE pstDate, const char *kpszDate, char *pszSeparator, ENUM_DATE_FMT eDateFmt) {
+  char szTemp[5] = "";
+  
+  memset(szTemp, 0x00, sizeof(szTemp));
+  
+  if ( !pstDate || !kpszDate )
+    return;
+
+  if ( !bStrIsEmpty(pszSeparator) ) {
+    switch ( eDateFmt ) {
+      case DDMMYYYY:
+      case DDMMYY:
+      case YYYYMMDD:
+      case YYMMDD:
+      case MMDDYYYY:
+      case MMDDYY: {
+        int iDiaPos = 0;
+        int iMesPos = 0;
+        int iAnoPos = 0;
+        if ( eDateFmt == DDMMYYYY || eDateFmt == DDMMYY ){
+          iDiaPos = 0;
+          iMesPos = 3;
+          iAnoPos = 6;
+        }
+        else if ( eDateFmt == YYYYMMDD || eDateFmt == YYMMDD ) {
+          iDiaPos = 6;
+          iMesPos = 4;
+          iAnoPos = 0;
+        }
+        else {
+          iDiaPos = 3;
+          iMesPos = 0;
+          iAnoPos = 6;
+        }
+        szTemp[0] = kpszDate[iDiaPos];
+        szTemp[1] = kpszDate[iDiaPos + 1];
+        szTemp[2] = '\0';
+	pstDate->iDay = atoi(szTemp);
+        szTemp[0] = kpszDate[iMesPos];
+        szTemp[1] = kpszDate[iMesPos + 1];
+        szTemp[2] = '\0';
+        pstDate->iMonth = atoi(szTemp);
+        if ( eDateFmt == DDMMYYYY || eDateFmt == YYYYMMDD || eDateFmt == MMDDYYYY ) {
+          int ii = 0;
+          for ( ii = 0; ii < 4; ii++ )
+            szTemp[ii] = kpszDate[iAnoPos + ii];
+          szTemp[4] = '\0';
+        }
+        else {
+          szTemp[0] = kpszDate[iAnoPos];
+          szTemp[1] = kpszDate[iAnoPos + 1];
+          szTemp[2] = '\0';
+        }
+        pstDate->iYear = atoi(szTemp);
+        break;
+      }
+      case DATE_FMTERROR:
+      default:
+        return;
+    }
+  }
+  else {
+    switch ( eDateFmt ) {
+      case DDMMYYYY: {
+        int ii = 0;
+        szTemp[0] = kpszDate[0];
+        szTemp[1] = kpszDate[1];
+        szTemp[2] = '\0';
+        pstDate->iDay = atoi(szTemp);
+        szTemp[0] = kpszDate[2];
+        szTemp[1] = kpszDate[3];
+	szTemp[2] = '\0';
+        pstDate->iMonth = atoi(szTemp);
+        for ( ii = 0; ii < 4; ii++ )
+          szTemp[ii] = kpszDate[4 + ii];
+        szTemp[4] = '\0';
+        pstDate->iYear = atoi(szTemp);
+        break;
+      }
+      case DDMMYY: {
+        szTemp[0] = kpszDate[0];
+        szTemp[1] = kpszDate[1];
+        szTemp[2] = '\0';
+        pstDate->iDay = atoi(szTemp);
+        szTemp[0] = kpszDate[2];
+        szTemp[1] = kpszDate[3];
+        szTemp[2] = '\0';
+        pstDate->iMonth = atoi(szTemp);
+        szTemp[0] = kpszDate[4];
+        szTemp[1] = kpszDate[5];
+        szTemp[2] = '\0';
+        pstDate->iYear = atoi(szTemp);
+        break;
+      }
+      case YYYYMMDD: {
+        int ii = 0;
+        for ( ii = 0; ii < 4; ii++ )
+          szTemp[ii] = kpszDate[ii];
+        szTemp[4] = '\0';
+        pstDate->iYear = atoi(szTemp);
+        szTemp[0] = kpszDate[4];
+        szTemp[1] = kpszDate[5];
+        szTemp[2] = '\0';
+        pstDate->iMonth = atoi(szTemp);
+	szTemp[0] = kpszDate[6];
+        szTemp[1] = kpszDate[7];
+        szTemp[2] = '\0';
+        pstDate->iDay = atoi(szTemp);
+        break;
+      }
+      case YYMMDD: {
+        szTemp[0] = kpszDate[0];
+        szTemp[1] = kpszDate[1];
+        szTemp[2] = '\0';
+        pstDate->iYear = atoi(szTemp);
+        szTemp[0] = kpszDate[2];
+        szTemp[1] = kpszDate[3];
+        szTemp[2] = '\0';
+        pstDate->iMonth = atoi(szTemp);
+        szTemp[0] = kpszDate[4];
+        szTemp[1] = kpszDate[5];
+        szTemp[2] = '\0';
+        pstDate->iDay = atoi(szTemp);
+        break;
+      }
+      case MMDDYYYY: {
+        int ii = 0;
+        szTemp[0] = kpszDate[0];
+        szTemp[1] = kpszDate[1];
+        szTemp[2] = '\0';
+        pstDate->iMonth = atoi(szTemp);
+        szTemp[0] = kpszDate[2];
+        szTemp[1] = kpszDate[3];
+        szTemp[2] = '\0';
+        pstDate->iDay = atoi(szTemp);
+        for ( ii = 0; ii < 4; ii++ )
+          szTemp[ii] = kpszDate[4 + ii];
+        szTemp[4] = '\0';pstDate->iYear = atoi(szTemp);
+        break;
+      }
+      case MMDDYY: {
+        szTemp[0] = kpszDate[0];
+        szTemp[1] = kpszDate[1];
+        szTemp[2] = '\0';
+        pstDate->iMonth = atoi(szTemp);
+        szTemp[0] = kpszDate[2];
+        szTemp[1] = kpszDate[3];
+        szTemp[2] = '\0';
+        pstDate->iDay = atoi(szTemp);
+        szTemp[0] = kpszDate[4];
+        szTemp[1] = kpszDate[5];
+        szTemp[2] = '\0';
+        pstDate->iYear = atoi(szTemp);
+        break;
+      }
+      case DATE_FMTERROR:
+      default:
+        return;
+    }
+  }
+}
+
+void vFormatTime(
+  const PSTRUCT_TIME kpszTime,
+  const ENUM_TIME_FMT eTimeFmt,
+  char *pszOutput,
+  size_t lOutputSize) {
+  switch ( eTimeFmt ) {
+    case HHMM: {
+      snprintf(pszOutput, lOutputSize, "%02d:%02d", kpszTime->iHour, kpszTime->iMinute);
+      break;
+    }
+    case HHMMSS: {
+      snprintf(pszOutput, lOutputSize, "%02d:%02d:%02d", kpszTime->iHour, kpszTime->iMinute, kpszTime->iSecond);
+      break;
+    }
+    case HHMMSSM: {
+      snprintf(pszOutput, lOutputSize, "%02d:%02d:%02d:%3d", kpszTime->iHour, kpszTime->iMinute, kpszTime->iSecond, kpszTime->iMillisecond);
+      break;
+    }
+    case TIME_FMTERROR:
     default:
-      szOutput = NULL;
       return;
   }
 }
 
-void vPrintDate(PSTRUCT_DATE pstDate, const char *kpszFmt)
-{
-  char szOutput[256];
-
-  memset(szOutput, 0, sizeof(szOutput));
-
-  vFormatDate(pstDate, kpszFmt, szOutput, "/");
-
+void vPrintTime(PSTRUCT_TIME pstTime, ENUM_TIME_FMT eTimeFmt) {
+  char szOutput[256] = "";
+  memset(szOutput, 0x00, sizeof(szOutput));
+  vFormatTime(pstTime, eTimeFmt, szOutput, sizeof(szOutput));
   puts(szOutput);
+}
+
+void vPrintDateTime(PSTRUCT_DATE pstDate, ENUM_DATE_FMT eDateFmt, const char *kpszDateSeparator, PSTRUCT_TIME pstTime, ENUM_TIME_FMT eTimeFmt) {
+  char szDateOutput[256] = "";
+  char szTimeOutput[256] = "";
+  memset(szDateOutput, 0x00, sizeof(szDateOutput));
+  memset(szTimeOutput, 0x00, sizeof(szTimeOutput));
+  vFormatDate(pstDate, eDateFmt, kpszDateSeparator, szDateOutput, sizeof(szDateOutput));
+  vFormatTime(pstTime, eTimeFmt, szTimeOutput, sizeof(szTimeOutput));
+  printf("%s %s\n", szDateOutput, szTimeOutput);
 }
 
 bool bTimeIsValid(STRUCT_TIME *pstTime)
